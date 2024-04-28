3183fd9523a791b0198463b2629893809a858dea
sliman4 <4sliman4@gmail.com>
2024-04-28 07:07:35 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,217 @@
+#[cfg(test)]
+mod tests;
+
+use async_trait::async_trait;
+use inindexer::fastnear_data_server::FastNearDataServerProvider;
+use inindexer::indexer_utils::{
+    EventLogData, NftBurnEvent, NftBurnLog, NftMintEvent, NftMintLog, NftTransferEvent,
+    NftTransferLog,
+};
+use inindexer::near_indexer_primitives::types::AccountId;
+use inindexer::near_indexer_primitives::CryptoHash;
+use inindexer::near_indexer_primitives::StreamerMessage;
+use inindexer::{
+    run_indexer, BlockIterator, CompletedTransaction, Indexer, IndexerOptions,
+    PreprocessTransactionsSettings,
+};
+use redis::aio::MultiplexedConnection;
+use redis::streams::StreamMaxlen;
+use redis::AsyncCommands;
+
+#[async_trait]
+trait NftEventHandler: Send + Sync {
+    async fn handle_mint(&mut self, mint: NftMintEvent, context: EventContext);
+    async fn handle_transfer(&mut self, transfer: NftTransferEvent, context: EventContext);
+    async fn handle_burn(&mut self, burn: NftBurnEvent, context: EventContext);
+}
+
+struct NftIndexer<T: NftEventHandler + Send + Sync + 'static> {
+    handler: T,
+}
+
+#[async_trait]
+impl<T: NftEventHandler + Send + Sync + 'static> Indexer for NftIndexer<T> {
+    type Error = String;
+
+    async fn on_transaction(
+        &mut self,
+        transaction: &CompletedTransaction,
+        _block: &StreamerMessage,
+    ) -> Result<(), Self::Error> {
+        for receipt in transaction.receipts.iter() {
+            let get_context_lazy = || {
+                let sender_id = receipt.receipt.receipt.predecessor_id.clone();
+                let contract_id = receipt.receipt.receipt.receiver_id.clone();
+                let txid = transaction.transaction.transaction.hash;
+                let block_height = receipt.block_height;
+                EventContext {
+                    txid,
+                    block_height,
+                    sender_id,
+                    contract_id,
+                }
+            };
+            if receipt.is_successful(false) {
+                for log in &receipt.receipt.execution_outcome.outcome.logs {
+                    if !log.contains("nep171") {
+                        // Don't even start parsing logs if they don't even contain the NEP-171 standard
+                        continue;
+                    }
+                    if let Ok(mint_log) = EventLogData::<NftMintLog>::deserialize(log) {
+                        if mint_log.validate() {
+                            log::debug!("Mint log: {mint_log:?}");
+                            for mint in mint_log.data.0 {
+                                self.handler.handle_mint(mint, get_context_lazy()).await;
+                            }
+                        }
+                    }
+                    if let Ok(transfer_log) = EventLogData::<NftTransferLog>::deserialize(log) {
+                        if transfer_log.validate() {
+                            log::debug!("Transfer log: {transfer_log:?}");
+                            for transfer in transfer_log.data.0 {
+                                self.handler
+                                    .handle_transfer(transfer, get_context_lazy())
+                                    .await;
+                            }
+                        }
+                    }
+                    if let Ok(burn_log) = EventLogData::<NftBurnLog>::deserialize(log) {
+                        if burn_log.validate() {
+                            log::debug!("Burn log: {burn_log:?}");
+                            for burn in burn_log.data.0 {
+                                self.handler.handle_burn(burn, get_context_lazy()).await;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+struct PushToRedisStream {
+    connection: MultiplexedConnection,
+    max_blocks: usize,
+}
+
+#[async_trait]
+impl NftEventHandler for PushToRedisStream {
+    async fn handle_mint(&mut self, mint: NftMintEvent, context: EventContext) {
+        for token_id in mint.token_ids {
+            let response: String = self
+                .connection
+                .xadd_maxlen(
+                    "nft_mint",
+                    StreamMaxlen::Approx(self.max_blocks),
+                    &format!("{}-*", context.block_height),
+                    &[
+                        ("owner_id", mint.owner_id.as_str()),
+                        ("token_id", token_id.as_str()),
+                        ("memo", mint.memo.as_deref().unwrap_or("")),
+                        ("txid", context.txid.to_string().as_str()),
+                        ("block_height", context.block_height.to_string().as_str()),
+                        ("sender_id", context.sender_id.as_str()),
+                        ("contract_id", context.contract_id.as_str()),
+                    ],
+                )
+                .await
+                .unwrap();
+            log::debug!("Adding to stream: {response}");
+        }
+    }
+
+    async fn handle_transfer(&mut self, transfer: NftTransferEvent, context: EventContext) {
+        for token_id in transfer.token_ids {
+            let response: String = self
+                .connection
+                .xadd_maxlen(
+                    "nft_transfer",
+                    StreamMaxlen::Approx(self.max_blocks),
+                    &format!("{}-*", context.block_height),
+                    &[
+                        ("old_owner_id", transfer.old_owner_id.as_str()),
+                        ("new_owner_id", transfer.new_owner_id.as_str()),
+                        ("token_id", token_id.as_str()),
+                        ("memo", transfer.memo.as_deref().unwrap_or("")),
+                        ("txid", context.txid.to_string().as_str()),
+                        ("block_height", context.block_height.to_string().as_str()),
+                        ("sender_id", context.sender_id.as_str()),
+                        ("contract_id", context.contract_id.as_str()),
+                    ],
+                )
+                .await
+                .unwrap();
+            log::debug!("Adding to stream: {response}");
+        }
+    }
+
+    async fn handle_burn(&mut self, burn: NftBurnEvent, _context: EventContext) {
+        for token_id in burn.token_ids {
+            let response: String = self
+                .connection
+                .xadd_maxlen(
+                    "nft_burn",
+                    StreamMaxlen::Approx(self.max_blocks),
+                    &format!("{}-*", _context.block_height),
+                    &[
+                        ("owner_id", burn.owner_id.as_str()),
+                        ("token_id", token_id.as_str()),
+                        ("memo", burn.memo.as_deref().unwrap_or("")),
+                        ("txid", _context.txid.to_string().as_str()),
+                        ("block_height", _context.block_height.to_string().as_str()),
+                        ("sender_id", _context.sender_id.as_str()),
+                        ("contract_id", _context.contract_id.as_str()),
+                    ],
+                )
+                .await
+                .unwrap();
+            log::debug!("Adding to stream: {response}");
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct EventContext {
+    pub txid: CryptoHash,
+    pub block_height: u64,
+    pub sender_id: AccountId,
+    pub contract_id: AccountId,
+}
+
+#[tokio::main]
+async fn main() {
+    simple_logger::SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .with_module_level("inindexer::performance", log::LevelFilter::Debug)
+        .init()
+        .unwrap();
+
+    let client = redis::Client::open(
+        std::env::var("REDIS_URL").expect("No $REDIS_URL environment variable set"),
+    )
+    .unwrap();
+    let connection = client.get_multiplexed_tokio_connection().await.unwrap();
+
+    let mut indexer = NftIndexer {
+        handler: PushToRedisStream {
+            connection,
+            max_blocks: 10_000,
+        },
+    };
+
+    run_indexer(
+        &mut indexer,
+        FastNearDataServerProvider::mainnet(),
+        IndexerOptions {
+            range: BlockIterator::iterator(117_189_143..=117_189_146),
+            preprocess_transactions: Some(PreprocessTransactionsSettings {
+                postfetch_blocks: 0,
+                ..Default::default()
+            }),
+            ..Default::default()
+        },
+    )
+    .await
+    .expect("Indexer run failed");
+}
---
+++ src/tests.rs
@@ -0,0 +1,211 @@
+use std::collections::HashMap;
+
+use async_trait::async_trait;
+use inindexer::{
+    fastnear_data_server::FastNearDataServerProvider,
+    indexer_utils::{NftBurnEvent, NftMintEvent, NftTransferEvent},
+    near_indexer_primitives::types::AccountId,
+    run_indexer, BlockIterator, IndexerOptions, PreprocessTransactionsSettings,
+};
+
+use crate::{EventContext, NftEventHandler, NftIndexer};
+
+#[tokio::test]
+async fn detects_mints() {
+    struct TestHandler {
+        mint_events: HashMap<AccountId, Vec<(NftMintEvent, EventContext)>>,
+    }
+
+    #[async_trait]
+    impl NftEventHandler for TestHandler {
+        async fn handle_mint(&mut self, mint: NftMintEvent, context: EventContext) {
+            self.mint_events
+                .entry(context.sender_id.clone())
+                .or_insert_with(Vec::new)
+                .push((mint, context));
+        }
+
+        async fn handle_transfer(&mut self, _transfer: NftTransferEvent, _context: EventContext) {}
+
+        async fn handle_burn(&mut self, _burn: NftBurnEvent, _context: EventContext) {}
+    }
+
+    let handler = TestHandler {
+        mint_events: HashMap::new(),
+    };
+
+    let mut indexer = NftIndexer { handler };
+
+    run_indexer(
+        &mut indexer,
+        FastNearDataServerProvider::mainnet(),
+        IndexerOptions {
+            range: BlockIterator::iterator(117_189_143..=117_189_146),
+            preprocess_transactions: Some(PreprocessTransactionsSettings {
+                prefetch_blocks: 0,
+                postfetch_blocks: 0,
+            }),
+            ..Default::default()
+        },
+    )
+    .await
+    .unwrap();
+
+    assert_eq!(
+        *indexer
+            .handler
+            .mint_events
+            .get(&"minter1.sharddog.near".parse::<AccountId>().unwrap())
+            .unwrap(),
+        vec![(
+            NftMintEvent {
+                owner_id: "slimedragon.near".parse().unwrap(),
+                token_ids: vec!["19:23".to_owned()],
+                memo: None
+            },
+            EventContext {
+                txid: "9TkiwECEL4AMsA6KmuhGskkNFT5Mr6ub6YJJAza8vbGs"
+                    .parse()
+                    .unwrap(),
+                block_height: 117189144,
+                sender_id: "minter1.sharddog.near".parse().unwrap(),
+                contract_id: "claim.sharddog.near".parse().unwrap()
+            }
+        )]
+    );
+}
+
+#[tokio::test]
+async fn detects_transfers() {
+    struct TestHandler {
+        transfer_events: HashMap<AccountId, Vec<(NftTransferEvent, EventContext)>>,
+    }
+
+    #[async_trait]
+    impl NftEventHandler for TestHandler {
+        async fn handle_mint(&mut self, _mint: NftMintEvent, _context: EventContext) {}
+
+        async fn handle_transfer(&mut self, transfer: NftTransferEvent, context: EventContext) {
+            let entry = self
+                .transfer_events
+                .entry(context.sender_id.clone())
+                .or_insert_with(Vec::new);
+            entry.push((transfer, context));
+        }
+
+        async fn handle_burn(&mut self, _burn: NftBurnEvent, _context: EventContext) {}
+    }
+
+    let handler = TestHandler {
+        transfer_events: HashMap::new(),
+    };
+
+    let mut indexer = NftIndexer { handler };
+
+    run_indexer(
+        &mut indexer,
+        FastNearDataServerProvider::mainnet(),
+        IndexerOptions {
+            range: BlockIterator::iterator(117_487_093..=117_487_095),
+            preprocess_transactions: Some(PreprocessTransactionsSettings {
+                prefetch_blocks: 0,
+                postfetch_blocks: 0,
+            }),
+            ..Default::default()
+        },
+    )
+    .await
+    .unwrap();
+
+    assert_eq!(
+        *indexer
+            .handler
+            .transfer_events
+            .get(&"slimegirl.near".parse::<AccountId>().unwrap())
+            .unwrap(),
+        vec![(
+            NftTransferEvent {
+                authorized_id: None,
+                old_owner_id: "slimegirl.near".parse().unwrap(),
+                new_owner_id: "tattothetoo.near".parse().unwrap(),
+                token_ids: vec!["504983:1".to_owned()],
+                memo: None
+            },
+            EventContext {
+                txid: "95HkmF7ajYPSSJnhsGL7C4k8sF5jmdrp4ciiTcK7xuYr"
+                    .parse()
+                    .unwrap(),
+                block_height: 117_487_094,
+                sender_id: "slimegirl.near".parse().unwrap(),
+                contract_id: "x.paras.near".parse().unwrap()
+            }
+        )]
+    );
+}
+
+#[tokio::test]
+async fn detects_burns() {
+    struct TestHandler {
+        burn_events: HashMap<AccountId, Vec<(NftBurnEvent, EventContext)>>,
+    }
+
+    #[async_trait]
+    impl NftEventHandler for TestHandler {
+        async fn handle_mint(&mut self, _mint: NftMintEvent, _context: EventContext) {}
+
+        async fn handle_transfer(&mut self, _transfer: NftTransferEvent, _context: EventContext) {}
+
+        async fn handle_burn(&mut self, burn: NftBurnEvent, context: EventContext) {
+            self.burn_events
+                .entry(context.sender_id.clone())
+                .or_insert_with(Vec::new)
+                .push((burn, context));
+        }
+    }
+
+    let handler = TestHandler {
+        burn_events: HashMap::new(),
+    };
+
+    let mut indexer = NftIndexer { handler };
+
+    run_indexer(
+        &mut indexer,
+        FastNearDataServerProvider::mainnet(),
+        IndexerOptions {
+            range: BlockIterator::iterator(117_752_571..=117_752_573),
+            preprocess_transactions: Some(PreprocessTransactionsSettings {
+                prefetch_blocks: 0,
+                postfetch_blocks: 0,
+            }),
+            ..Default::default()
+        },
+    )
+    .await
+    .unwrap();
+
+    eprintln!("{:?}", indexer.handler.burn_events);
+    assert_eq!(
+        *indexer
+            .handler
+            .burn_events
+            .get(&"bonehedz.near".parse::<AccountId>().unwrap())
+            .unwrap(),
+        vec![(
+            NftBurnEvent {
+                owner_id: "bonehedz.near".parse().unwrap(),
+                authorized_id: None,
+                token_ids: vec!["1454".to_owned()],
+                memo: None
+            },
+            EventContext {
+                txid: "9k7kE7PU1YqrAxzdwKw8P3u8eNeazCZpMWStD89XFBpZ"
+                    .parse()
+                    .unwrap(),
+                block_height: 117752572,
+                sender_id: "bonehedz.near".parse().unwrap(),
+                contract_id: "veganfriends.mintbase1.near".parse().unwrap()
+            }
+        )]
+    );
+}
